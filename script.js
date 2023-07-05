6532b713723654c941a01c870a7605dbacdf6c18
L.J Solis <l.jsolis@L.lan>
2023-07-05 12:14:52 -0500

    re-arranged the order of my code feels nicer to me this way.

--- script.js
+++ script.js
@@ -6,22 +6,15 @@ function generatePassword() {
   var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", "{", "}", "|", ";", ":", "?", "/", "<", ">", ".", ",", "~"];
   var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-  // Variable that will be used to obtain the password length.
-
-  var Length = getLength();
+  var numberSet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
   // Empty array that will be used to store the arrays above and allow for characters to be selected at random.
 
   var passwordArray = [];
 
-  // Allows users to confirm what characters they want in their password.
+  // Variable that will be used to obtain the password length.
 
-  var special = confirm("Would you like your password to contain any special characters?");
-  var upper = confirm("Would you like your password to contain any uppercase letters?");
-  var lower = confirm("Would you like your password to contain any lowercase letters?");
-  var num = confirm("Would you like your password to contain any numbers?");
+  var Length = getLength();
 
   // Function ensures the user creates a password between 8-128 characters. 
   //It also makes sure the user enters a number value and not a letter resetting it back to zero if it is a letter.
@@ -38,6 +31,14 @@ function generatePassword() {
   
     return characterLength;
   }
+
+  // Allows users to confirm what characters they want in their password.
+
+  var special = confirm("Would you like your password to contain any special characters?");
+  var upper = confirm("Would you like your password to contain any uppercase letters?");
+  var lower = confirm("Would you like your password to contain any lowercase letters?");
+  var number = confirm("Would you like your password to contain any numbers?");
+
   // Creating an array that will be used to generate the password by randomly selecting from it.
 
   if (special) {
@@ -52,8 +53,8 @@ function generatePassword() {
     passwordArray = passwordArray.concat(lowerCaseLetters);
   }
 
-  if (num) {
-    passwordArray = passwordArray.concat(number);
+  if (number) {
+    passwordArray = passwordArray.concat(numberSet);
   }
   // Loop will generate a random password based upon the users choices.
 
