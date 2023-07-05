92d48b10d8092d47cb3a7316b1d8351e21606eef
L.J Solis <l.jsolis@L.lan>
2023-07-05 11:07:24 -0500

    Added a function that will allow the user to set thier password length within the given parameters.

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-// Get references to the #generate element
-
-var generateBtn = document.querySelector("#generate");
-
 // Assignment code here
 function generatePassword() {
 
-  // Created arrays for every character type able to be used within the password perimeters.
+  // Created arrays for every character type able to be used within the password parameters.
 
   var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", "{", "}", "|", ";", ":", "?", "/", "<", ">", ".", ",", "~"];
   var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-  var passwordLength = getLength();
+  var passwordLength = getPasswordLength();
 
-  // empty array
+  // empty array that will be used to store the arrays above.
 
   var emptyArray = [];
 
@@ -24,6 +20,21 @@ function generatePassword() {
   var upper = confirm("Would you like your password to contain any uppercase letters?");
   var lower = confirm("Would you like your password to contain any lowercase letters?");
   var num = confirm("Would you like your password to contain any numbers?");
+}
+  // Function ensures the user creates a password between 8-128 characters. 
+  //It also makes sure the user enter a number and not a letter resetting it back to zero if it is a letter.
+
+function getPasswordLength() {
+  var characterLength = 0;
+  while ((characterLength < 8) || (characterLength > 128)) {
+    characterLength = window.prompt("Password should be between 8 - 128 characters");
+
+    if (isNaN(characterLength)) {
+      characterLength = 0;
+    }
+  }
+  
+  return characterLength;
 }
   // Applying all the character arrays into the empty array that was created.
 
@@ -43,8 +54,13 @@ if (num) {
   emptyArray = emptyArray.concat(number);
 }
 
+for (var i = 0; i < passwordLength; i++) {
+  
+}
 
+// Get references to the #generate element
 
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
