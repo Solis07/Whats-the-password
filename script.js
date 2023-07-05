b9e9fedfffae92cfd037fe0234400e6ef83ea68e
L.J Solis <l.jsolis@L.lan>
2023-07-05 10:32:53 -0500

    Applied all characters arrays to the empty array that was created.

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function generatePassword() {
 
   // Created arrays for every character type able to be used within the password perimeters.
 
-  var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", "{", "}", "|", ";", ":", "?", "/", "<", ">", ".", ",", "~"];
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", "{", "}", "|", ";", ":", "?", "/", "<", ">", ".", ",", "~"];
+  var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-  var passwordLength = get
+  var passwordLength = getLength();
 
   // empty array
 
@@ -23,21 +23,28 @@ function generatePassword() {
   var special = confirm("Would you like your password to contain any special characters?");
   var upper = confirm("Would you like your password to contain any uppercase letters?");
   var lower = confirm("Would you like your password to contain any lowercase letters?");
-  var number = confirm("Would you like your password to contain any numbers?");
-
+  var num = confirm("Would you like your password to contain any numbers?");
+}
   // Applying all the character arrays into the empty array that was created.
 
-  
-
-
-
-
-
-
-
-
-
+if (special) {
+  emptyArray = emptyArray.concat(specialCharacters);
 }
+
+if (upper) {
+  emptyArray = emptyArray.concat(upperCaseLetters);
+}
+
+if (lower) {
+  emptyArray = emptyArray.concat(lowerCaseLetters);
+}
+
+if (num) {
+  emptyArray = emptyArray.concat(number);
+}
+
+
+
 
 // Write password to the #password input
 
